6a8d2209e461b11f444be7d95d1b8ddd817c0e2d
Ashwani1207 <57230745+Ashwani1207@users.noreply.github.com>
2020-05-25 18:42:23 +0530

    Typecast for age variable
    Age should be in integer

--- DAY 1/Input_Print.py
+++ DAY 1/Input_Print.py
@@ -2,7 +2,7 @@ name = input("Enter your name : ")
 branch = input("Enter your branch : ")
 gender = input("Enter your gender : ")
 college = input("Enter your college name : ")
-age = input("Enter your age : ")
+age = int(input("Enter your age : "))
 
 print("\n NAME :",name)
 print("\n BRANCH :",branch)
